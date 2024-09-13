c1aed7efd034dc94f2004302b3ed28e625638eee
Max Murakami-Moses <110478230+maxMurakamiMoses@users.noreply.github.com>
2024-09-13 01:17:46 -0400

    authroized user can created leaderboard

---
+++ src/app/api/leaderboard/route.ts
@@ -0,0 +1,38 @@
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+
+export async function POST(req: Request) {
+  try {
+    const session = await getAuthSession()
+
+    if (session?.user.email !== "max.murakamimoses24@gmail.com") {
+        return new Response('Unauthorized', { status: 401 })
+    }
+
+    const body = await req.json()
+    const { name } = body
+
+    // check if leaderboard already exists
+    const leaderboardExists = await db.leaderboard.findFirst({
+      where: {
+        name,
+      },
+    })
+
+    if (leaderboardExists) {
+      return new Response('Subreddit already exists', { status: 409 })
+    }
+
+    // create leaderboard and associate it with the user
+    const leaderboard = await db.leaderboard.create({
+      data: {
+        name,
+        creatorId: session.user.id,
+      },
+    })
+
+    return new Response(leaderboard.name)
+  } catch (error) {
+    return new Response('Could not create subreddit', { status: 500 })
+  }
+}
