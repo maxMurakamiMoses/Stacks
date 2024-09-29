c1116b7172cf9720c529ae8706f9ec61951f3485
Max Murakami-Moses <110478230+maxMurakamiMoses@users.noreply.github.com>
2024-09-28 23:06:23 -0400

    added about hero section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,12 @@
+import { Hero } from '@/components/about/Hero'
+import News from '@/components/landing/News'
 import React from 'react'
 
 const page = () => {
   return (
     <>
-    
+        <Hero />
+        <News />
     </>
   )
 }
