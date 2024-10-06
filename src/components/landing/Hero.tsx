bcd9c3e7c018a2ac5a08ce70d564c9dde2dfd8e2
Max Murakami-Moses <110478230+maxMurakamiMoses@users.noreply.github.com>
2024-10-06 02:10:14 -0400

    better landing hero

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,13 +1,13 @@
 "use client";
-import ArrowIcon from '@/assets/arrow-right-black.svg';
-import Image from 'next/image';
 import Link from 'next/link';
+import { FaArrowRight } from 'react-icons/fa'; // Import FaArrowRight
 import { useRef } from 'react';
 import { CardStackSection } from '../leaderboards/CardStack';
 import { TypewriterEffect } from "../ui/typewriter-effect"; // Import TypewriterEffect
 import { Roboto_Mono } from 'next/font/google';
 import { Grid } from './Grid';
 import { LogoTicker } from './Logoticker';
+import TailwindcssButtons from './TailwindcssButtons';
 
 const robotoMono = Roboto_Mono({
   subsets: ['latin'],
@@ -41,10 +41,16 @@ export const Hero = () => {
                         Biohacker, nerd or athlete? <br /> This is for you. <br />
                     </p>
                     <div className="flex gap-1 items-center mt-[30px]">
-                        <Link href="/about" className="btn btn-primary tracking-tight text-[#e0fcc4]">Learn More</Link>
-                        <Link href="/leaderboards" className="flex items-center text-black bg-transparent group tracking-tight pl-6 text-[#e0fcc4]">
+                        {/* Updated Link with border, rounded corners, and hover effect */}
+                        <Link 
+                            href="/about" 
+                            className="btn btn-primary tracking-tight text-[#e0fcc4] border border-[#e0fcc4] rounded-xl px-2 py-1 hover:font-bold transition font-normal"
+                        >
+                            Learn More
+                        </Link>
+                        <Link href="/leaderboards" className="flex items-center bg-transparent group tracking-tight pl-6 text-[#e0fcc4]">
                             <span>Jump To Leaderboard</span>
-                            <Image src={ArrowIcon} alt="Arrow right" className="h-5 w-5 ml-1 transform transition-transform group-hover:translate-x-2"/>
+                            <FaArrowRight className="h-5 w-5 ml-2 transform transition-transform group-hover:translate-x-2" />
                         </Link>
                     </div>
                 </div>
